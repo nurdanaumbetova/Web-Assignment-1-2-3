881711de54d40aab1634ef1eae84e339881ff5a9
nurdanaumbetova <144439854+nurdanaumbetova@users.noreply.github.com>
2024-11-12 22:37:20 +0500

    Lab7

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-const sounds = {
-    C: new Audio('sounds/C.wav'),
-    D: new Audio('sounds/D.wav'),
-    E: new Audio('sounds/E.wav'),
-    F: new Audio('sounds/F.wav'),
-    G: new Audio('sounds/G.wav'),
-    A: new Audio('sounds/A.wav'),
-    B: new Audio('sounds/B.wav')
-};
+let heroImage = document.getElementById("hero-image");
+let heroName = document.getElementById("hero-name");
+let heroUniverse = document.getElementById("hero-universe");
+let heroStats = document.getElementById("hero-stats");
 
-function playSound(note) {
-    if (sounds[note]) {
-        sounds[note].currentTime = 0;
-        sounds[note].play();
+let heroData;
 
-        const keyElement = document.getElementById(note);
-        keyElement.classList.add('active');
-        setTimeout(() => keyElement.classList.remove('active'), 100); 
+async function loadHeroes() {
+    let url = "https://akabab.github.io/superhero-api/api/all.json";
+    try {
+        let response = await fetch(url);
+        heroData = await response.json();
+        getRandomHero();
+    } catch (error) {
+        console.error("Ошибка при загрузке данных о героях:", error);
     }
 }
 
-document.querySelectorAll('.key').forEach((key) => {
-    key.addEventListener('click', () => {
-        const note = key.getAttribute('data-sound');
-        playSound(note);
-    });
-});
+function getRandomHero() {
+    if (!heroData) return;
 
-document.addEventListener('keydown', (event) => {
-    const note = event.key.toUpperCase();
-    if (sounds[note]) {
-        playSound(note);
-    }
-});
+    let randomHero = heroData[Math.floor(Math.random() * heroData.length)];
+
+
+    heroName.innerText = randomHero.name;
+    heroUniverse.innerText = `Origin Universe: ${randomHero.biography.publisher || "Unknown"}`;
+    heroImage.src = randomHero.images.md;
+
+    heroStats.innerHTML = `
+        <p class="hero-stat"><strong>Alignment:</strong> ${randomHero.biography.alignment}</p>
+        <p class="hero-stat"><strong>Occupation:</strong> ${randomHero.work.occupation || "Unknown"}</p>
+        <p class="hero-stat"><strong>Power:</strong> ${randomHero.powerstats.power || "Not Available"}</p>
+        <p class="hero-stat"><strong>Strength Level:</strong> ${randomHero.powerstats.strength || "Unknown"}</p>
+        <p class="hero-stat"><strong>Agility:</strong> ${randomHero.powerstats.speed || "N/A"}</p>
+    `;
+}
+window.addEventListener("load", loadHeroes);
